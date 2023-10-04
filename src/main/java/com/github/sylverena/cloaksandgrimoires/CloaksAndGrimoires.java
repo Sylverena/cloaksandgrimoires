34a14a79015f2faee433b2cf3989a1a15e24fb40
Cybestry <cybestryz@gmail.com>
2023-10-04 10:53:41 -0500

    Added entry point CloaksAndGrimoires.java and modified fabric.mod.json file to have the former as an entry point. Added init package for registering all things that our mod uses

---
+++ src/main/java/com/github/sylverena/cloaksandgrimoires/CloaksAndGrimoires.java
@@ -0,0 +1,14 @@
+package com.github.sylverena.cloaksandgrimoires;
+
+/**
+ * Class CloaksAndGrimoires
+ */
+public class CloaksAndGrimoires implements net.fabricmc.api.ModInitializer
+{
+
+    @Override
+    public void onInitialize()
+    {
+
+    }
+}
