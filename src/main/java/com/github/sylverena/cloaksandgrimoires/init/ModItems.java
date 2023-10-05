f33f6967132a918fa167d0bc6ac9170278cebf0f
Cybestry <cybestryz@gmail.com>
2023-10-04 19:57:28 -0500

    Added lang file for item display names, and added an item group for all mod items

--- src/main/java/com/github/sylverena/cloaksandgrimoires/init/ModItems.java
+++ src/main/java/com/github/sylverena/cloaksandgrimoires/init/ModItems.java
@@ -1,10 +1,15 @@
 package com.github.sylverena.cloaksandgrimoires.init;
 
 import com.github.sylverena.cloaksandgrimoires.CloaksAndGrimoires;
+import com.github.sylverena.cloaksandgrimoires.items.GrimoireItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 /**
@@ -12,16 +17,26 @@ import net.minecraft.util.Identifier;
  */
 public class ModItems
 {
-    public static Item BAREBONES_GRIMOIRE;
-/*    public static final Item BASIC_GRIMOIRE;
-    public static final Item GOLD_GRIMOIRE;*/
+    public static GrimoireItem BAREBONES_GRIMOIRE;
+
+    /**
+     * This Itemgroup should house all the items
+     */
+    private static final ItemGroup CLOAKS_AND_GRIMOIRES_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(BAREBONES_GRIMOIRE))
+            .displayName(Text.translatable("itemgroup.cloaksandgrimoires.cloaksandgrimoires_group"))
+            .entries(((displayContext, entries) ->
+                    entries.add(BAREBONES_GRIMOIRE)))
+            .build();
 
     /**
      * Method init() registers all the items to the registry
      */
     public static void init()
     {
-        BAREBONES_GRIMOIRE = Registry.register(Registries.ITEM, new Identifier(CloaksAndGrimoires.MODID, "barebones_grimoire"), new Item(new FabricItemSettings()));
+        Registry.register(Registries.ITEM_GROUP, new Identifier(CloaksAndGrimoires.MODID, "cloaksandgrimoires_group"), CLOAKS_AND_GRIMOIRES_GROUP);
+
+        BAREBONES_GRIMOIRE = Registry.register(Registries.ITEM, new Identifier(CloaksAndGrimoires.MODID, "barebones_grimoire"), new GrimoireItem(new FabricItemSettings()));
     }
 
 
